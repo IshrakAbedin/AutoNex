e89b6f38a5eafbfbcb0703670a2b9d55c1f7d43d
Mohammad Ishrak Abedin <ishrak.abedin.a1@gmail.com>
2025-09-17 00:27:47 +0600

    Implemented the StateMachine

--- include/autonex/StateMachine.hpp
+++ include/autonex/StateMachine.hpp
@@ -6,21 +6,16 @@
 
 #include "EventDispatcher.hpp"
 
-enum class Dummy
-{
-	STATE1, STATE2, STATE3
-};
-
 namespace anx {
 	template<typename state_t>
 	class StateMachine
 	{
 	public:
-		using rule_t = std::function<bool>;
-		using rule_state_pair = std::pair<rule_t, state_t>;
-		using transition_rule_list_t = std::vector<rule_state_pair>;
+		using rule_t = std::function<bool()>;
+		using rule_state_pair_t = std::pair<rule_t, state_t>;
+		using transition_rule_list_t = std::vector<rule_state_pair_t>;
 		using transition_registry_t = std::unordered_map<state_t, transition_rule_list_t>;
-		
+
 		using transition_action_t = std::function<void(state_t)>;
 		using transition_event_dispatcher_t = EventDispatcher<state_t>;
 		using transition_event_registry_t = std::unordered_map<state_t, transition_event_dispatcher_t>;
@@ -40,7 +35,7 @@ namespace anx {
 
 		void AddTranstionRule(state_t from, state_t to, rule_t rule);
 		void BindOnStateLeave(state_t from, transition_action_t action);
-		void BindOnStateEntry(state_t from, transition_action_t action);
+		void BindOnStateEntry(state_t into, transition_action_t action);
 
 		bool Step();
 	};
@@ -48,32 +43,63 @@ namespace anx {
 	template<typename state_t>
 	inline state_t StateMachine<state_t>::GetCurrentState() const
 	{
-		return state_t();
+		return m_CurrentState;
 	}
 
 	template<typename state_t>
 	inline void StateMachine<state_t>::ForceSetCurrentState(state_t newState)
 	{
+		m_CurrentState = newState;
 	}
 
 	template<typename state_t>
 	inline void StateMachine<state_t>::AddTranstionRule(state_t from, state_t to, rule_t rule)
 	{
+		m_RuleRegistry[from].emplace_back(rule, to);
 	}
 
 	template<typename state_t>
 	inline void StateMachine<state_t>::BindOnStateLeave(state_t from, transition_action_t action)
 	{
+		m_OnStateLeaveRegistry[from] << action;
 	}
 
 	template<typename state_t>
-	inline void StateMachine<state_t>::BindOnStateEntry(state_t from, transition_action_t action)
+	inline void StateMachine<state_t>::BindOnStateEntry(state_t into, transition_action_t action)
 	{
+		m_OnStateEntryRegistry[into] << action;
 	}
 
 	template<typename state_t>
 	inline bool StateMachine<state_t>::Step()
 	{
+		auto it = m_RuleRegistry.find(m_CurrentState);
+		if (it != m_RuleRegistry.end())
+		{
+			for (const auto& [rule, newState] : it->second)
+			{
+				if (rule())
+				{
+					// Check and fire on leave events
+					if (auto it_l = m_OnStateLeaveRegistry.find(m_CurrentState);
+						it_l != m_OnStateLeaveRegistry.end())
+					{
+						it_l->second.Dispatch(newState);
+					}
+
+					// Check and fire on entry events
+					if (auto it_e = m_OnStateEntryRegistry.find(newState);
+						it_e != m_OnStateEntryRegistry.end())
+					{
+						it_e->second.Dispatch(m_CurrentState);
+					}
+
+					// Transit
+					m_CurrentState = newState;
+					return true;
+				}
+			}
+		}
 		return false;
 	}
-}
+} // namespace anx
--- test/Application.cpp
+++ test/Application.cpp
@@ -1,10 +1,105 @@
 #include <iostream>
 #include <format>
 
+#include <string>
+#include <vector>
+
 #include "StateMachine.hpp"
+
+#define println(fmt, ...) std::cout << std::format(fmt, __VA_ARGS__) <<std::endl
+
+enum class States
+{
+	ALIVE, INJURED, DEAD, IMMORTAL
+};
+
+template <>
+struct std::formatter<States> : std::formatter<std::string> {
+	auto format(States st, std::format_context& ctx) const {
+		std::string s;
+		switch (st) {
+		case States::ALIVE:		s = "<Alive>";	break;
+		case States::INJURED:	s = "<Injured>";	break;
+		case States::DEAD:		s = "<Dead>";		break;
+		case States::IMMORTAL:	s = "<Immortal>"; break;
+		default:				s = "<Unknown>";	break;
+		}
+		return std::formatter<std::string>::format(s, ctx);
+	}
+};
 
 int main()
 {
-	std::cout << std::format("Hello, {}", 123) << std::endl;
+	anx::StateMachine<States> sm{States::ALIVE};
+	int health{ 100 };
+	bool immortality{ false };
+
+	sm.AddTranstionRule(States::ALIVE, States::INJURED,
+		[&]() { return health < 50; }
+	);
+	sm.AddTranstionRule(States::ALIVE, States::IMMORTAL,
+		[&]() { return immortality; }
+	);
+
+	sm.AddTranstionRule(States::INJURED, States::ALIVE,
+		[&]() { return health >= 50; }
+	);
+	sm.AddTranstionRule(States::INJURED, States::IMMORTAL,
+		[&]() { return immortality; }
+	);
+	sm.AddTranstionRule(States::INJURED, States::DEAD,
+		[&]() { return health <= 0; }
+	);
+
+	sm.AddTranstionRule(States::IMMORTAL, States::ALIVE,
+		[&]() { return health >= 50 && !immortality; }
+	);
+
+	sm.AddTranstionRule(States::IMMORTAL, States::INJURED,
+		[&]() { return health < 50 && !immortality; }
+	);
+
+	sm.AddTranstionRule(States::IMMORTAL, States::DEAD,
+		[&]() { return health <= 0 && !immortality; }
+	);
+
+	println("Starting with state: {}", sm.GetCurrentState());
+	println("Iterating 9 times to deduct health by 10 and then stepping");
+	for (int i = 0; i < 9; i++)
+	{
+		health -= 10;
+		println("Current health: {}", health);
+		if (i == 2)
+		{
+			println("Activating immortality");
+			immortality = true;
+		}
+		else if (i == 4)
+		{
+			println("Deactivating immortality");
+			immortality = false;
+		}
+		sm.Step();
+		println("Current state: {}\n", sm.GetCurrentState());
+	}
+	println("Iterating 10 times to increase health by 10 and then stepping");
+	for (int i = 0; i < 10; i++)
+	{
+		health += 10;
+		println("Current health: {}", health);
+		if (i == 1)
+		{
+			println("Activating immortality");
+			immortality = true;
+		}
+		else if (i == 2)
+		{
+			println("Deactivating immortality");
+			immortality = false;
+		}
+		sm.Step();
+		println("Current state: {}\n", sm.GetCurrentState());
+	}
+
 	return 0;
 }
